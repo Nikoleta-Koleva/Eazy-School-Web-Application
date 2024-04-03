fa13db975cee35a3d1f255190df3ef8791cf06b4
Nikoleta <up899244@myport.ac.uk>
2024-04-03 22:50:49 +0100

    Added login, dashboard page, controllers and configuration for Spring security in ProjectSecurityConfig file
    User and admin roles available, and dashboard is shown after they login

---
+++ src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.eazybytes.eazyschool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+@Configuration
+public class ProjectSecurityConfig {
+    /*Overrides the default security settings in the defaultSecurityFilterChain method in the SpringBootWebSecurityConfiguration class
+    assets/**, indicates that everything inside the assets folder is permitted, holidays/** permits all requests for holiday path
+    permitAll() or .denyAll() methods can be used to permit/deny all HTTP authorization requests
+    Thymeleaf form action="/saveMsg" method="post" protects from CSRF (Cross Site Request Forgery) attacks by showing error 403
+    */
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").authenticated()
+                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/holidays/**").permitAll()
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/saveMsg").permitAll()
+                        .requestMatchers("/courses").permitAll()
+                        .requestMatchers("/about").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/assets/**").permitAll())
+                .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
+                        .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
+                .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
+                        .invalidateHttpSession(true).permitAll())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    //Configure multiple users inside the memory of the web application
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("54321")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
